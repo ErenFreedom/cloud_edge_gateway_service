e652e86fc0a882eb652436872606ed6b4ffce4d8
konceptEngineersDev <commissioning@konceptengineers.com>
2026-05-27 11:49:13 +0530

    Fixed dg sensor uuid mismatch for Griha

--- backend/src/modules/griha/griha.bigquery.repository.ts
+++ backend/src/modules/griha/griha.bigquery.repository.ts
@@ -9,13 +9,13 @@ export const getMonthlyConsumptionFromBQ = async (
 ) => {
 
   const query = `
-  SELECT consumption AS total_consumption
-  FROM \`project-b5045c0e-60ef-4535-bc3.cloud_edge_gateway_master_data.griha_final_clean\`
-  WHERE sensor_id = @sensorId
-    AND month = @month
-    AND year = @year
-  LIMIT 1
-`;
+    SELECT consumption AS total_consumption
+    FROM \`project-b5045c0e-60ef-4535-bc3.cloud_edge_gateway_master_data.griha_final_consumption\`
+    WHERE sensor_id = @sensorId
+      AND month = @month
+      AND year = @year
+    LIMIT 1
+  `;
 
   const [rows] = await bigquery.query({
     query,
